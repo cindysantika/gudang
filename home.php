50547fe346991fef2aece00dc53d1ede1e50d7f3
rismarizkyramadhani <rismarizkyramadhani95@gmail.com>
2015-07-09 11:48:48 +0700

    slide

--- home.php
+++ home.php
@@ -87,6 +87,70 @@ if (!empty($_GET['error'])) {
 </td>
 
 <td width="350"> Isi Web
+<style type='text/css'>
+    #slideshow img
+    {
+        position: absolute;
+        left: 0;
+        top: 0;
+    }
+    </style>
+ 
+    <!--jquery, bisa didownload di jquery.com
+    // atau ambil dari google CDN (seperti dibawah)-->
+    <script type='text/javascript'
+        src='http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'>
+    </script>
+ 
+    <!--script animasi-->
+    <script type='text/javascript'>
+ 
+    // Fungsi yang menjadi inti dari script animasi ini
+    function simpleSlideshow(slideContainer, duration)
+    {
+        // gambar/foto yang akan dianimasikan
+        // adalah child (anak) pertama dari container yang digunakan
+        var currentSlide = $('img:nth-child(1)', slideContainer);
+ 
+        // slide tersebut akan dimodifikasi sedemikian rupa
+        $(currentSlide)
+        .css({
+            opacity: 0 // buat opacity-nya menjadi 0, sehingga tidak terlihat (transparan)
+        })
+        // appendTo akan membuat anak pertama menjadi anak terakhir,
+        // sehingga akan berada dibagian paling depan.
+        // namun meskipun berada dipaling depan,
+        // tidak akan terlihat karena sudah transparan (step sebelumnya)
+        .appendTo(slideContainer)
+ 
+        // animasikan tingkat opacity menjadi 1 (FULL),
+        // sehingga pelan-pelan akan terlihat
+        .animate({
+            opacity: 1
+        }, 'normal', function(){
+            setTimeout(function(){
+                // fungsi akan dijalankan kembali
+                // setelah sekian detik sesuai dengan duration
+                // sehingga menampilkan efek animasi berulang-ulang
+                simpleSlideshow(slideContainer, duration);
+            }, duration);
+        })
+    }
+ 
+    // implementasi
+    // jalankan fungsi animasi ketika document sudah di-load (ready)
+    $(function(){
+        var duration = 3000; // millsecond        
+        var slideContainer = $('#slideshow');
+        simpleSlideshow(slideContainer, duration);
+    });
+    </script>
+<div id='slideshow'>
+    <img src='img/1.jpg'>
+    <img src='img/2.jpg'>
+    <img src='img/3.jpg'>
+
+</div>
 </td></tr>
 <tr><td colspan="2" height="100"> <img src="Gambar.jpg" height="100" width="900" /></td></tr>
 </table>
